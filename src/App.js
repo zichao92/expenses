2f90d13d65154e7416e90e3eaf3ee54e636b3b84
ZiChao Toh <zichao.toh@foodpanda.com>
2021-07-25 19:45:22 +0800

    Uploading

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from './components/expenseItem';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2> Let's get started</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/expenseItem.js
@@ -0,0 +1,13 @@
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+      </div>
+      <div>Amount</div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
